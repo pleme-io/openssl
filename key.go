c857300408a2169250ec97252a67e2d2d9e08d58
Dmitry Gorochovsky <d.goro@yahoo.com>
2022-07-31 15:21:39 +0300

    test

--- key.go
+++ key.go
@@ -480,9 +480,9 @@ func GenerateECKey(curve EllipticCurve) (PrivateKey, error) {
 	// 	return nil, errors.New(fmt.Sprintf("failed setting curve in EC parameter generation context. Error code: %v, curve: %v", errCode, curve))
 	// }
 
-	var iCurve = 519
+	var iCurve = 480
 	var errCode int
-	for ; iCurve <= 551; iCurve++ {
+	for ; iCurve <= 510; iCurve++ {
 		errCode = int(C.X_EVP_PKEY_CTX_set_ec_paramgen_curve_nid(paramCtx, C.int(iCurve)))
 		if errCode == 1 {
 			return nil, errors.New(fmt.Sprintf("URA setting curve in EC parameter generation context. curve: %v", iCurve))
